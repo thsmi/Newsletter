47dbd64bd523de2904743c9ce7eb385b68701d15
Thomas Schmid <thomas-schmid@gmx.net>
2017-01-06 02:07:53 +0100

    cleanup and documents settings.php

--- php/newsletter/settings/settings.php
+++ php/newsletter/settings/settings.php
@@ -1,10 +1,26 @@
 <?php
 
+/**
+ * Reads and writes preferences. 
+ *
+ * Settings are identified by a unique cannonical path. 
+ * The path is separated by dots.
+ *
+ * For perforance reasonse the settings are split into separate files.
+ * The first path segement defines the json file which contains the setting. 
+ */
 class Settings {
     
     private static $items = [];    
-    
-    
+
+    /**
+     * Reads the given perference. 
+     * The preferences are cached they are loaded for each request only once.
+     *
+     * @param $path the preference which should be read
+     *
+     * @return the preference read
+     */
     static function getProperty($path) {
         $path = explode(".",$path,2);
         
@@ -16,6 +32,14 @@ class Settings {
         return Settings::$items[$path[0]][$path[1]];
     }
     
+    /**
+     * Writes the given prefernce to file. The preference is written imediately.
+     * Existing entries are replaced silently.
+     *
+     * @param $path the preference which should be written
+     * @param $value the value which should be saved
+     * 
+     */
     static function setProperty($path, $value) {
         $path = explode(".",$path,2);
         Settings::$items[$path[0]] = json_decode(file_get_contents("settings/".$path[0].".json"), true);
@@ -26,31 +50,5 @@ class Settings {
     }
     
 }
-// Show
-// cryptokey
-
-// Path
-//  AddressBook
-//  Drafts
-//  Archive
-//  Template
-
-// Mail
-//  Prefix [FGWH]
-//  From [newsletter@wolf-hirth.de]
-//  Reply TO [newsletter@wolf-hirth.de]
-//
-// [] sendmail
-//
-// [] smtp
-//   Host
-//   Secure Auth
-//   Secure = "TLS|SSL|NONE"
-//   PORT
-//   Username
-//   Password
-
-// admin users
-
 
 ?>
